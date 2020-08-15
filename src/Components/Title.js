676e9b2b2dbea0e68f9460c0e9db1d914a5e3971
Abelard Bautista <abelardbautista@gmail.com>
2020-08-15 02:00:06 -0700

    Moved title to separate component

---
+++ src/Components/Title.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Emoji from "./Emoji";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Title = () => {
+  return (
+    <Row style={{ zIndex: "1" }}>
+      <Col>
+        <h1 style={{ textAlign: "center" }}>
+          {" "}
+          <Emoji symbol={"✈️"} label="airplane" />
+          Time Until Microsoft Flight Simulator 2020
+          <Emoji symbol={"✈️"} label="airplane" />
+        </h1>
+      </Col>
+    </Row>
+  );
+};
+
+export default Title;
