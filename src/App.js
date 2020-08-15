267aae28cff4b71bb8cd71429a5e04a7a287f374
Abelard Bautista <abelardbautista@gmail.com>
2020-08-14 20:17:37 -0700

    Added title and emojis.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Countdown from "./Components/Countdown";
+import Emoji from "./Components/Emoji";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {" "}
+        <Emoji symbol={"✈️"} label="airplane" />
+        Time Until Microsoft Flight Simulator 2020
+        <Emoji symbol={"✈️"} label="airplane" />
+      </h1>
+      <Countdown />
     </div>
   );
 }
