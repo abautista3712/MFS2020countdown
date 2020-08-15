eac229c7d46ec660c7c2bd58547a2f0c38c758b6
Abelard Bautista <abelardbautista@gmail.com>
2020-08-15 01:04:55 -0700

    Added video background w/ text overlay.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from "react";
 import Countdown from "./Components/Countdown";
 import Emoji from "./Components/Emoji";
-import logo from "./logo.svg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        {" "}
-        <Emoji symbol={"✈️"} label="airplane" />
-        Time Until Microsoft Flight Simulator 2020
-        <Emoji symbol={"✈️"} label="airplane" />
-      </h1>
-      <Countdown />
+    <div>
+      <video
+        style={{ position: "absolute", minHeight: "100%", minWidth: "100%" }}
+        loop
+        autoPlay
+      >
+        <source
+          src="https://assets.mixkit.co/videos/preview/mixkit-hyperlapse-of-a-cloudy-sky-1766-large.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <Container className="centerAlign">
+        <Row style={{ zIndex: "1" }}>
+          <Col>
+            <h1 style={{ textAlign: "center" }}>
+              {" "}
+              <Emoji symbol={"✈️"} label="airplane" />
+              Time Until Microsoft Flight Simulator 2020
+              <Emoji symbol={"✈️"} label="airplane" />
+            </h1>
+          </Col>
+        </Row>
+        <Countdown />
+      </Container>
     </div>
   );
 }
